b759ef77f16f2481acaa5ef354cfedc88f4a6b78
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-16 23:52:19 +0300

    style: styling for dropdown menu

--- weather/src/components/SearchBar/index.js
+++ weather/src/components/SearchBar/index.js
@@ -2,8 +2,7 @@ import React, { useState } from 'react'
 import { useContext } from 'react'
 import Sehir from '../../contexts/SearchContext/SearchContext'
 import turkiye from './turkiye'
-import styles from './style.module.css'
-
+import './styles.css'
 
 function SearchBar() {
   const {city,setCity} = useContext(Sehir)
@@ -14,8 +13,7 @@ function SearchBar() {
   return (
     
     <div>
-      <label htmlFor='sehirSecimi'>Şehir: </label>
-      <select className='m-auto' id='sehirSecimi' onChange={handleChange} value={city}>
+      <select className='dropdown my-3' id='sehirSecimi' onChange={handleChange} value={city}>
         {turkiye.map((item)=>{
           return(
             <option key={item.id} value={item.value} >{item.name}</option>
