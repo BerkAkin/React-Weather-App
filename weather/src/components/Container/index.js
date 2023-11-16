8d3a8e4af520df670ce7ac732b33004bdd0197cb
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-16 22:59:13 +0300

    fix: container dark mode support

--- weather/src/components/Container/index.js
+++ weather/src/components/Container/index.js
@@ -1,54 +1,15 @@
 import React from 'react'
-import SearchBar from '../SearchBar';
-import Temperature from '../Temperature';
-import { useContext } from 'react';
-import { SehirProvider } from '../../contexts/SearchContext/SearchContext';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import SubContainer from '../SubContainer';
+
+import { ThemeProvider } from '../../contexts/ThemeContext/ThemeContext';
 
 function Container() {
   return (
     <div>
-      <SehirProvider>
-        <div className='container-fluid'>
-          <div className='row'style={{height:'100vh'}}>
-
-
-
-            <div className='col-3' >
-
-
-
-              {/* ARAMA KUTUSUNUN SEKMESİ */}
-              <div className='row bg-success' style={{height:'10vh'}}>
-                  <SearchBar/>
-              </div>
-              {/* ARAMA KUTUSUNUN SEKMESİ */}
-
-
-
-
-
-
-              {/* MEVCUT GÜN  */}
-              <div className='row bg-danger' style={{height:'60vh'}}>
-                <Temperature/>
-              </div>
-              {/* MEVCUT GÜN  */}
-
-
-
-              
-              <div className='row bg-warning' style={{height:'30vh'}}>DETAYLAR</div>
-            </div>
-
-
-
-            <div className='col-3 border'>24 SAATLİK VERİ</div>
-            <div className='col-6 border'>DİĞER GÜNLER</div>
-          </div>
-        </div>
-      </SehirProvider>
+    <ThemeProvider>
+      <SubContainer/>
+    </ThemeProvider>
     </div>
   )
 }
---
+++ weather/src/components/SubContainer/index.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+import SearchBar from '../SearchBar';
+import Temperature from '../Temperature';
+import { SehirProvider } from '../../contexts/SearchContext/SearchContext';
+import Options from '../Options';
+
+import { useContext } from 'react';
+import Theme from '../../contexts/ThemeContext/ThemeContext'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css'
+
+function SubContainer() {
+    const {theme} = useContext(Theme)
+    console.log(theme)
+  return (
+    <div>
+        <SehirProvider>
+
+          <div className='container-fluid'>
+            <div className='row' style={{height:'10vh'}}>
+                <div className={`col-3 secim${theme}`}><SearchBar/></div>
+                <div className={`col-9 settings${theme}`}><Options/></div>
+            </div>
+            <div className='row' style={{height:'90vh'}}>
+            <div className={`col-3 current${theme}`}><Temperature/></div>
+            <div className={`col-2 hourly${theme}`}></div>
+            <div className={`col-7 daily${theme}`}></div>
+            </div>
+          </div>
+          
+      </SehirProvider>
+    </div>
+  )
+}
+
+export default SubContainer
