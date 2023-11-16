0ab676bce8ffaeb147633f109fb208dcf3d5cffc
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-17 00:57:31 +0300

    style: topcontext for current data

--- weather/src/components/Temperature/index.js
+++ weather/src/components/Temperature/index.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import { useContext } from 'react'
 import Sehir from '../../contexts/SearchContext/SearchContext'
+import Theme from '../../contexts/ThemeContext/ThemeContext'
+import './style.css'
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Temperature() {
   const {city,setCity} = useContext(Sehir)
+  const {theme} = useContext(Theme)
+
   return (
-    <div>{city}</div>
+    <>
+      <div className={`container-fluid rounded topContext${theme}`} style={{height:'88vh'}}>
+      <div style={{height:'50vh'}}>
+        <div className='row text-center'><span><img height="200" src="/cloud.png"></img></span></div>
+          <div className='row mt-3 text-center' style={{fontSize:'1.6em'}}><p>Parçalı Bulutlu</p></div>
+          <div className='row justify-content-center'><p className='border border-dark rounded-pill w-25 text-center' style={{fontSize:'1.6em'}}>15 °C</p></div>
+          <div className='row '><p className=' text-center'>Hissedilen Sıcaklık : 13 °C</p></div>
+        </div>
+        <hr/>
+         <div className='row' style={{height:'33vh'}}></div>
+      </div>
+      
+     
+    </>
   )
 }
 
