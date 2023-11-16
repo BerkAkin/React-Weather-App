453c615d675418d92228a5b4e91a7b659e8416ff
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-16 22:58:46 +0300

    feat: added dark mode support

---
+++ weather/src/contexts/ThemeContext/ThemeContext.js
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+
+const Theme  = createContext();
+
+export const ThemeProvider = ({children})=>{
+
+    const [theme,setTheme] = useState('Light')
+
+    const toggleTheme = ()=>{
+        setTheme(theme==='Light'?'Dark':'Light');
+    }
+
+    const values = {
+        theme,
+        toggleTheme
+    }
+    return(
+        <Theme.Provider value={values}>
+            {children}
+        </Theme.Provider>
+    )
+}
+
+
+export default Theme;
