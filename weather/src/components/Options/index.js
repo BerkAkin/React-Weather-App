428a489736e9cb03344443bed47d4d5a0ba67d23
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-16 23:40:02 +0300

    style: button style for mode swap

--- weather/src/components/Options/index.js
+++ weather/src/components/Options/index.js
@@ -3,15 +3,27 @@ import { useContext } from 'react'
 import Theme from '../../contexts/ThemeContext/ThemeContext'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function Options() {
   const {theme,toggleTheme} = useContext(Theme)
   return (
-    <div>
-      <div className="form-check form-switch">
-        <input className="form-check-input" type="checkbox" role="switch" id="darkSwitch" onChange={toggleTheme} />
-        <label className="form-check-label" htmlFor="darkSwitch">Dark Mode</label>
+    <>
+      <div className='h-100'>
+        <div className='row'>
+          <div className='col-9'>
+          </div>
+          <div className='col-3'>
+            <div className="form-check form-switch">
+              <input className="form-check-input d-none" type="checkbox" role="switch" id="darkSwitch" onChange={toggleTheme} />
+              <label className="form-check-label py-2 float-end " htmlFor="darkSwitch"><img width="50" src={`/${theme}.png`}></img></label>
+            </div>
+          </div>
+          
+        </div>
+        
       </div>
-    </div>
+      
+    </>
   )
 }
 
