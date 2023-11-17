ce2325efdf00e5fd7f8f643ae956448b2ccca1a1
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-17 04:01:53 +0300

    remove: widget components

--- weather/src/components/Temperature/index.js
+++ weather/src/components/Temperature/index.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { useContext } from 'react'
 import Sehir from '../../contexts/SearchContext/SearchContext'
 import Theme from '../../contexts/ThemeContext/ThemeContext'
+import Details from '../Details'
 import './style.css'
-import Humid from '../Details/Humid'
-import Temps from '../Details/Temps'
-import Wind from '../Details/Wind'
-import Sunrise from '../Details/Sunrise'
+
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -18,31 +17,18 @@ function Temperature() {
   return (
     <>
       <div className={`container-fluid rounded topContext${theme}`} style={{height:'88vh'}}>
-      <div style={{height:'50vh'}}>
+      <div style={{height:'45vh'}}>
         <div className='row text-center'><span><img height="200" src="/cloud.png"></img></span></div>
-          <div className='row mt-3 text-center' style={{fontSize:'1.6em'}}><p>Parçalı Bulutlu</p></div>
+          <div className='row mt-2 text-center' style={{fontSize:'1.6em'}}><p>Parçalı Bulutlu</p></div>
           <div className='row justify-content-center'><p className='border border-dark rounded-pill w-25 text-center' style={{fontSize:'1.6em'}}>15 °C</p></div>
           <div className='row '><p className=' text-center'>Hissedilen Sıcaklık : 13 °C</p></div>
         </div>
         <hr/>
 
 
-         <div className='container-fluid ' style={{height:'33vh'}}>
+         <div style={{height:'35vh'}}>
 
-         <div className='row  h-50'>
-            <div className='col-6 '><Humid/></div>
-            <div className='col-6 '><Sunrise/></div>
-          </div>
-          <div className='row  h-50'>
-            <div className='col-6 '><Temps/></div>
-            <div className='col-6 '><Wind/></div>
-          </div>
-
-          
-          
-          
-          
-
+          <Details/>
 
          </div>
 
