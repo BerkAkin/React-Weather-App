4bfb68f5bb773d282bdb15ccfa1ebecbba980bf9
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-19 02:54:59 +0300

    style: container fixed

--- weather/src/components/Temperature/index.js
+++ weather/src/components/Temperature/index.js
@@ -14,15 +14,15 @@ function Temperature() {
 
   return (
     <>
-      <div className={`container-fluid rounded topContext${theme}`} style={{height:'88vh'}}>
-      <div style={{height:'45vh'}}>
+      <div className={`rounded-start topContext${theme}`} style={{height:'85.7vh'}}>
+      <div style={{height:'41vh'}}>
         <div className='row text-center'><span><img height="200" src="/cloud.png"></img></span></div>
           <div className='row mt-2 text-center' style={{fontSize:'1.6em'}}><p>Parçalı Bulutlu</p></div>
           <div className='row justify-content-center'><p className='border border-dark rounded-pill w-25 text-center' style={{fontSize:'1.6em'}}>15 °C</p></div>
-          <div className='row '><p className=' text-center'>Hissedilen Sıcaklık : 13 °C</p></div>
+          <div className='row'><p className=' text-center'>Hissedilen Sıcaklık : 13 °C</p></div>
         </div>
         <hr/>
-         <div style={{height:'35vh'}}>
+         <div style={{height:'37vh'}}>
           <Details/>
          </div>
       </div>
