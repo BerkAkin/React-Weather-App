1ff504da9d736c640bf147f362868dff5c3a4277
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-19 04:28:11 +0300

    style: next days are now half

--- weather/src/components/NextDays/index.js
+++ weather/src/components/NextDays/index.js
@@ -6,39 +6,46 @@ function NextDays() {
   const {theme} = useContext(Theme)
   return (
     <>
-      <div className={`rounded mt- container fullCard${theme}`}>
-        <div className='row 'style={{height:'16vh'}}>
+      <div className={`container `}>
+        <div className='row mt-2'style={{height:'17vh'}}>
 
-          <div className={`col-4 rounded-start align-items-center d-flex justify-content-center dailyLeftContainer${theme}`}>
-            <div className='display-4'>CUMARTESİ</div>
+
+
+          <div className={`col p-0 rounded-start`}>
+            <div className='row h-25'></div>
+            <div className={`rounded-start d-flex justify-content-evenly align-items-center dailyLeftContainer${theme}`}>
+              <img height={68} width={68} className='ms-1' src='/cloud.png'></img> 
+              <div className='col-1'></div>
+              <small className=' display-6'> 16°C</small>
+              <div className='col-1'></div>
+              <small className={` dailyContainerMidBottom${theme} text-center `}>Parçalı Bulutlu</small>
+            </div>
+            <div className='h-25'></div>
           </div>
 
-          <div className='col-4 p-0'>
-            <div className='d-flex align-items-center justify-content-center'>
-              <img height={90} width={90} src='/cloud.png'></img> <small className='display-6'> 16°C</small>
-            </div>
-            <div className='text-center'>
-              <div className='dailyContainerMidBottomDark'>Parçalı Bulutlu</div>
-            </div>
+
+          <div className={`col-4 h-75 rounded align-items-center d-flex justify-content-center fullCard${theme}`}>
+            <div className='display-6'>CUMARTESİ</div>
           </div>
 
-          <div className={`col-4 p-0 rounded-end dailyRightContainer${theme}`}>
-            <div className='container-fluid h-100'>
-              <div className='row text-center h-100'>
-                <div className='col-4 border-end'>
-                  <div className='row mt-3'><span><img width={50} src={`/hightemp${theme}.png`}></img></span></div>
-                  <div className='row mt-1'><span>Max</span></div>
-                  <div className='row'><span>20 °C</span></div>
+
+
+          <div className={`col p-0 rounded-end `}>
+            <div className={`container-fluid h-100`}>
+              <div className='row h-25'></div>
+              <div className={`row text-center rounded-end h-50 dailyRightContainer${theme}`}>
+
+                <div className={`col-4 lastBorder${theme}`}>
+                  <div className='row mt-2'><span><img width={30} src={`/hightemp${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>20 °C</span></div>
                 </div>
-                <div className='col-4 border-end'>
-                  <div className='row mt-3'><span><img width={50} src={`/humidity${theme}.png`}></img></span></div>
-                  <div className='row mt-1'><span>Nem</span></div>
-                  <div className='row'><span>%90</span></div>
+                <div className={`col-4 lastBorder${theme}`}>
+                  <div className='row mt-2'><span><img width={30} src={`/humidity${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>%90</span></div>
                 </div>
-                <div className='col-4'>
-                  <div className='row mt-3'><span><img width={50} src={`/sunrise${theme}.png`}></img></span></div>
-                  <div className='row mt-1'><span>Doğum</span></div>
-                  <div className='row'><span>07:45</span></div>
+                <div className={`col-4`}>
+                  <div className='row mt-2'><span><img width={30} src={`/sunrise${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>07:45</span></div>
                 </div>
               </div>
             </div>
@@ -47,6 +54,199 @@ function NextDays() {
 
         </div>
       </div> 
+      <div className={`container `}>
+        <div className='row mt-2'style={{height:'17vh'}}>
+
+
+
+          <div className={`col p-0 rounded-start`}>
+            <div className='row h-25'></div>
+            <div className={`rounded-start d-flex justify-content-evenly align-items-center dailyLeftContainer${theme}`}>
+              <img height={68} width={68} className='ms-1' src='/cloud.png'></img> 
+              <div className='col-1'></div>
+              <small className=' display-6'> 16°C</small>
+              <div className='col-1'></div>
+              <small className={` dailyContainerMidBottom${theme} text-center `}>Parçalı Bulutlu</small>
+            </div>
+            <div className='h-25'></div>
+          </div>
+
+
+          <div className={`col-4 h-75 rounded align-items-center d-flex justify-content-center fullCard${theme}`}>
+            <div className='display-6'>CUMARTESİ</div>
+          </div>
+
+
+
+          <div className={`col p-0 rounded-end `}>
+            <div className={`container-fluid h-100`}>
+              <div className='row h-25'></div>
+              <div className={`row text-center rounded-end h-50 dailyRightContainer${theme}`}>
+
+                <div className={`col-4 lastBorder${theme}`}>
+                  <div className='row mt-2'><span><img width={30} src={`/hightemp${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>20 °C</span></div>
+                </div>
+                <div className={`col-4 lastBorder${theme}`}>
+                  <div className='row mt-2'><span><img width={30} src={`/humidity${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>%90</span></div>
+                </div>
+                <div className={`col-4`}>
+                  <div className='row mt-2'><span><img width={30} src={`/sunrise${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>07:45</span></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div> 
+      <div className={`container `}>
+        <div className='row mt-2'style={{height:'17vh'}}>
+
+
+
+          <div className={`col p-0 rounded-start`}>
+            <div className='row h-25'></div>
+            <div className={`rounded-start d-flex justify-content-evenly align-items-center dailyLeftContainer${theme}`}>
+              <img height={68} width={68} className='ms-1' src='/cloud.png'></img> 
+              <div className='col-1'></div>
+              <small className=' display-6'> 16°C</small>
+              <div className='col-1'></div>
+              <small className={` dailyContainerMidBottom${theme} text-center `}>Parçalı Bulutlu</small>
+            </div>
+            <div className='h-25'></div>
+          </div>
+
+
+          <div className={`col-4 h-75 rounded align-items-center d-flex justify-content-center fullCard${theme}`}>
+            <div className='display-6'>CUMARTESİ</div>
+          </div>
+
+
+
+          <div className={`col p-0 rounded-end `}>
+            <div className={`container-fluid h-100`}>
+              <div className='row h-25'></div>
+              <div className={`row text-center rounded-end h-50 dailyRightContainer${theme}`}>
+
+                <div className={`col-4 lastBorder${theme}`}>
+                  <div className='row mt-2'><span><img width={30} src={`/hightemp${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>20 °C</span></div>
+                </div>
+                <div className={`col-4 lastBorder${theme}`}>
+                  <div className='row mt-2'><span><img width={30} src={`/humidity${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>%90</span></div>
+                </div>
+                <div className={`col-4`}>
+                  <div className='row mt-2'><span><img width={30} src={`/sunrise${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>07:45</span></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div> 
+      <div className={`container `}>
+        <div className='row mt-2'style={{height:'17vh'}}>
+
+
+
+          <div className={`col p-0 rounded-start`}>
+            <div className='row h-25'></div>
+            <div className={`rounded-start d-flex justify-content-evenly align-items-center dailyLeftContainer${theme}`}>
+              <img height={68} width={68} className='ms-1' src='/cloud.png'></img> 
+              <div className='col-1'></div>
+              <small className=' display-6'> 16°C</small>
+              <div className='col-1'></div>
+              <small className={` dailyContainerMidBottom${theme} text-center `}>Parçalı Bulutlu</small>
+            </div>
+            <div className='h-25'></div>
+          </div>
+
+
+          <div className={`col-4 h-75 rounded align-items-center d-flex justify-content-center fullCard${theme}`}>
+            <div className='display-6'>CUMARTESİ</div>
+          </div>
+
+
+
+          <div className={`col p-0 rounded-end `}>
+            <div className={`container-fluid h-100`}>
+              <div className='row h-25'></div>
+              <div className={`row text-center rounded-end h-50 dailyRightContainer${theme}`}>
+
+                <div className={`col-4 lastBorder${theme}`}>
+                  <div className='row mt-2'><span><img width={30} src={`/hightemp${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>20 °C</span></div>
+                </div>
+                <div className={`col-4 lastBorder${theme}`}>
+                  <div className='row mt-2'><span><img width={30} src={`/humidity${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>%90</span></div>
+                </div>
+                <div className={`col-4`}>
+                  <div className='row mt-2'><span><img width={30} src={`/sunrise${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>07:45</span></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div> 
+      <div className={`container `}>
+        <div className='row mt-2'style={{height:'17vh'}}>
+
+
+
+          <div className={`col p-0 rounded-start`}>
+            <div className='row h-25'></div>
+            <div className={`rounded-start d-flex justify-content-evenly align-items-center dailyLeftContainer${theme}`}>
+              <img height={68} width={68} className='ms-1' src='/cloud.png'></img> 
+              <div className='col-1'></div>
+              <small className=' display-6'> 16°C</small>
+              <div className='col-1'></div>
+              <small className={` dailyContainerMidBottom${theme} text-center `}>Parçalı Bulutlu</small>
+            </div>
+            <div className='h-25'></div>
+          </div>
+
+
+          <div className={`col-4 h-75 rounded align-items-center d-flex justify-content-center fullCard${theme}`}>
+            <div className='display-6'>CUMARTESİ</div>
+          </div>
+
+
+
+          <div className={`col p-0 rounded-end `}>
+            <div className={`container-fluid h-100`}>
+              <div className='row h-25'></div>
+              <div className={`row text-center rounded-end h-50 dailyRightContainer${theme}`}>
+
+                <div className={`col-4 lastBorder${theme}`}>
+                  <div className='row mt-2'><span><img width={30} src={`/hightemp${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>20 °C</span></div>
+                </div>
+                <div className={`col-4 lastBorder${theme}`}>
+                  <div className='row mt-2'><span><img width={30} src={`/humidity${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>%90</span></div>
+                </div>
+                <div className={`col-4`}>
+                  <div className='row mt-2'><span><img width={30} src={`/sunrise${theme}.png`}></img></span></div>
+                  <div><span className='fw-bold'>07:45</span></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div> 
+      
 
       
       
