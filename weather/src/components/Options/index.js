c722ef118fc203a335ec66c2493c9f83829ba843
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-21 02:57:54 +0300

    feat: added social buttons

--- weather/src/components/Options/index.js
+++ weather/src/components/Options/index.js
@@ -7,15 +7,26 @@ function Options() {
   const {theme,toggleTheme} = useContext(Theme)
   return (
     <>
-      <div className='h-100'>
+      <div className='container'>
         <div className='row'>
           <div className='col-9'>
           </div>
           <div className='col-3'>
-            <div className="form-check form-switch">
-              <input className="form-check-input d-none" type="checkbox" role="switch" id="darkSwitch" onChange={toggleTheme} />
-              <label className="form-check-label py-2 float-end " htmlFor="darkSwitch"><img width="50" src={`/${theme}.png`}></img></label>
+            <div className='row'>
+              <div className='col-4 d-flex justify-content-end align-items-center'>
+                <a href='https://github.com/BerkAkin' target='blank'><img className='rounded-pill' width={55} src='https://avatars.githubusercontent.com/berkakin'></img></a>
+              </div>
+              <div className='col-4 d-flex justify-content-start align-items-center'>
+                <a href='https://www.linkedin.com/in/berk-ak%C4%B1n-6a636121a/' target='blank'><img width={45} src={`/li${theme}.png`}></img></a>
+              </div>
+              <div className='col-4'>
+                <div className="form-check form-switch">
+                  <label className="form-check-label py-2" htmlFor="darkSwitch"><img width="45" src={`/${theme}.png`}></img></label>
+                  <input className="form-check-input d-none" type="checkbox" role="switch" id="darkSwitch" onChange={toggleTheme} />
+                </div>
+              </div>
             </div>
+            
           </div>
         </div>
       </div>
