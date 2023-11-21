ae46ab1b9089f7399b663f71e29cca350c3cb247
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-21 05:10:32 +0300

    not:

--- weather/src/components/Temperature/index.js
+++ weather/src/components/Temperature/index.js
@@ -15,27 +15,27 @@ function Temperature() {
   return (
     <>
       <div className={`rounded-start topContext${theme}`} style={{height:'86vh'}}>
-      <div style={{height:'41vh'}} className='d-flex p-2'>
-        <div className={`container rounded innerTab${theme}`}>
-          <div className='row h-50 text-center'>
-            <span>
-              <img height="200" src={data ? `http://openweathermap.org/img/wn/${data['list'][0]['weather'][0]['icon']}@4x.png`: " "}></img>
-            </span>
-          </div>          
-          <div className='row  mt-2 text-center' style={{fontSize:'1.6em', textTransform:'capitalize'}}>
-            <p>{data ? data['list'][0]['weather'][0]['description']:' '}</p>
+        <div style={{height:'41vh'}} className='d-flex p-2'>
+          <div className={`container rounded innerTab${theme}`}>
+            <div className='row h-50 text-center'>
+              <span>
+                <img height="200" src={data ? `http://openweathermap.org/img/wn/${data['list'][0]['weather'][0]['icon']}@4x.png`: " "}></img>
+              </span>
+            </div>          
+            <div className='row  mt-2 text-center' style={{fontSize:'1.6em', textTransform:'capitalize'}}>
+              <p>{data ? data['list'][0]['weather'][0]['description']:' '}</p>
+            </div>
+            <div className='row  justify-content-center'>
+              <p className='border rounded-pill w-25 text-center' style={{fontSize:'1.6em'}}>{data ? data['list'][0]['main']['temp'].toFixed(0):''} °C</p>
+            </div>
+            <div className='row '>
+              <p className=' text-center'>Hissedilen Sıcaklık : {data ? data['list'][0]['main']['feels_like'].toFixed(0):''} °C</p>
+            </div>
           </div>
-          <div className='row  justify-content-center'>
-            <p className='border rounded-pill w-25 text-center' style={{fontSize:'1.6em'}}>{data ? data['list'][0]['main']['temp'].toFixed(0):''} °C</p>
-          </div>
-          <div className='row '>
-            <p className=' text-center'>Hissedilen Sıcaklık : {data ? data['list'][0]['main']['feels_like'].toFixed(0):''} °C</p>
-          </div>
-        </div>
 
 
 
-        </div>
+          </div>
          <div className='mt-4' style={{height:'37vh'}}>
           <Details/>
          </div>
