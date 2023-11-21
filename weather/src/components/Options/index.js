4a1fde0bceab6a2bab60026a544e55bd8db13744
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-21 03:20:26 +0300

    update: names of files

--- weather/src/components/Options/index.js
+++ weather/src/components/Options/index.js
@@ -17,11 +17,11 @@ function Options() {
                 <a href='https://github.com/BerkAkin' target='blank'><img className='rounded-pill border' width={55} src='https://avatars.githubusercontent.com/berkakin'></img></a>
               </div>
               <div className='col-4 d-flex justify-content-start align-items-center'>
-                <a href='https://www.linkedin.com/in/berk-ak%C4%B1n-6a636121a/' target='blank'><img width={45} src={`/li${theme}.png`}></img></a>
+                <a href='https://www.linkedin.com/in/berk-ak%C4%B1n-6a636121a/' target='blank'><img width={45} src={`/linkedin${theme}.png`}></img></a>
               </div>
               <div className='col-4'>
                 <div className="form-check form-switch">
-                  <label className="form-check-label py-2" htmlFor="darkSwitch"><img width="45" src={`/${theme}.png`}></img></label>
+                  <label className="form-check-label py-2" htmlFor="darkSwitch"><img width="45" src={`/modSite${theme}.png`}></img></label>
                   <input className="form-check-input d-none" type="checkbox" role="switch" id="darkSwitch" onChange={toggleTheme} />
                 </div>
               </div>
