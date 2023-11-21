a01a1ffc0103f95da220f41faeaa1a11a8d38990
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-21 04:30:33 +0300

    style: made daily details thicker

--- weather/src/components/NextDays/index.js
+++ weather/src/components/NextDays/index.js
@@ -55,7 +55,7 @@ function NextDays() {
         <div key={item.id} className={`container mt-3 pt-1`}>
           <div className='row'style={{height:'17vh'}}>
 
-            <div className={`col-4 h-75 rounded-start rounded-top fullCard${theme}`}>
+            <div className={`col-4 h-75 rounded-start fullCard${theme}`}>
               <div className='container p-0  h-100'>
                 <div className='row h-100'>
                   <div className='col-8 d-flex align-items-center'>
@@ -77,36 +77,32 @@ function NextDays() {
 
             <div className={`col-4 p-0 `}>
               <div className='container-fluid h-100 p-0'>
-                <div className='h-25'></div>
-                <div className={`text-center h-50 d-flex justify-content-evenly align-items-center dailyLeftContainer${theme}`}>
-                  <div className='col-12zez'>
-                    <span style={{textTransform:'capitalize'}} className={`dailyContainerMidBottom${theme}`}>{item.weather}</span>
+                <div className={`h-75 dailyLeftContainer${theme}`}>
+                  <div className='col-12 h-100 d-flex align-items-center justify-content-center'>
+                    <span style={{textTransform:'capitalize', letterSpacing:2, fontSize:'20px'}} className={`dailyContainerMidBottom${theme}`}>{item.weather}</span>
                   </div>
                 </div>
-                <div className='h-25 '></div>
               </div>
             </div>
 
 
             <div className={`col-4 p-0 `}>
               <div className={`container-fluid h-100`}>
-                <div className='row h-25'></div>
-                <div className={`row h-50 text-center rounded-end dailyRightContainer${theme}`}>
+                <div className={`row h-75 text-center rounded-end dailyRightContainer${theme}`}>
 
                   <div className={`col-4`}>
-                    <div className='row mt-2'><span><img width={30} src={`/wind${theme}.png`}></img></span></div>
+                    <div className='row mt-4'><span><img width={40} src={`/wind${theme}.png`}></img></span></div>
                     <div><span className='fw-bold'>{item.wind} km/h</span></div>
                   </div>
                   <div className={`col-4`}>
-                    <div className='row mt-2'><span><img width={30} src={`/feel${theme}.png`}></img></span></div>
+                    <div className='row mt-4'><span><img width={40} src={`/feel${theme}.png`}></img></span></div>
                     <div><span className='fw-bold'>{item.feel.toFixed()}°C</span></div>
                   </div>
                   <div className={`col-4`}>
-                    <div className='row mt-2'><span><img width={30} src={`/clouds${theme}.png`}></img></span></div>
+                    <div className='row mt-4'><span><img width={40} src={`/clouds${theme}.png`}></img></span></div>
                     <div><span className='fw-bold'>% {item.clouds}</span></div>
                   </div>
                 </div>
-                <div className='h-25 '></div>
               </div>
             </div>
 
