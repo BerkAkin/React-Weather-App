ae14666a5acae76e0dd698b578882766f90dd8d7
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-26 02:45:03 +0300

    style: main container is now responsive

--- weather/src/components/SubContainer/index.js
+++ weather/src/components/SubContainer/index.js
@@ -20,19 +20,23 @@ function SubContainer() {
         <SehirProvider>
           <div className='container-fluid'>
             <div className='row' style={{height:'10vh'}}>
-                <div className={`col-3 secim${theme}`}><SearchBar/></div>
-                <div className={`col-9 settings${theme}`}><Options/></div>
+                <div className={`border col-xxl-3 col-xl-4 col-lg-6 col-md-6 col-sm-12 secim${theme}`}><SearchBar/></div>
+                <div className={`border col-xxl-9 col-xl-8 col-lg-6 col-md-6 d-sm-none d-md-block settings${theme}`}><Options/></div>
             </div>
 
             <div className='row' style={{height:'90vh'}}>
-              <div className={`col-5 current${theme}`}>
+
+
+              <div className={`col-xxl-5 col-xl-6 col-lg-12 col-md-12 col-sm-12 col-xs-1 border border-danger current${theme}`}>
                 <div className='row'>
-                  <div className='col-7 pe-0 rounded-start'><Temperature/></div>
-                  <div className={`hourly${theme} p-0 rounded-end col-5`} ><Hourly/></div>
+                  <div className='col-xl-7 col-md-6 pe-0 rounded-start'><Temperature/></div>
+                  <div className={`hourly${theme} p-0 col-md-6 col-xl-5 d-xl-block rounded-end`} ><Hourly/></div>
                 </div>
               </div>
-              <div className={`col-7 daily${theme}`}>
-                <div className='container  rounded' style={{height:'85.5vh'}}>
+
+
+              <div className={`col-xxl-7 col-xl-6 col-lg-12 col-md-12 col-sm-12 border border-success daily${theme}`}>
+                <div className='container rounded' style={{height:'85.5vh'}}>
                   <div className='row'><NextDays/></div>
                 </div>
               </div>
