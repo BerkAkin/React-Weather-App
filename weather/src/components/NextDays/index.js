55d8a2ab51bacc8c0e61797625f20d355b12b87e
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-26 05:38:56 +0300

    style:component is now fully responsive

--- weather/src/components/NextDays/index.js
+++ weather/src/components/NextDays/index.js
@@ -34,17 +34,15 @@ function NextDays() {
 
   return (
     <>
-      <div className='container'>
+      <div className='container mt-4'>
         <div className='row'>
-          <div className='col'><span className='display-5 '>Günlük Özet</span></div>
-          <div className='col d-flex align-items-center justify-content-center'>
-            
-          </div>
-          <div className='col'>
+          <div className='col-sm-10 col-md-8'><span className='display-5'>Günlük Özet</span></div>
+          <div className='col-md-4 d-sm-none d-none d-sm-block'></div>
+          <div className='col-sm-2 col-md-4 d-none d-sm-block'>
             <div className='row h-100 d-flex align-items-end text-center'>
-              <div className='col'><span>Rüzgar Hızı</span></div>
-              <div className='col'><span>Hissedilen</span></div>
-              <div className='col'><small>Bulut Oranı</small></div>
+              <div className='col-4 col-xl-6 col-xxl-4 col-sm-12 col-md-4 border'><span>Rüzgar</span></div>
+              <div className='col-4 col-xl-6 col-xxl-4 d-sm-none d-md-block col-md-4'><span>Hissedilen</span></div>
+              <div className='col-4 d-sm-block col-xxl-4 d-xxl-block d-md-block col-md-4 d-xl-none d-none d-sm-none'><small>Bulut Oranı</small></div>
             </div>
         </div>
         </div>
@@ -53,10 +51,14 @@ function NextDays() {
       {daysList.map((item)=>{
       return(
 
-        <div key={item.id} className={`container mt-3 pt-1`}>
+        <div key={item.id} className={`container mt-2 pt-1`}>
           <div className='row'style={{height:'17vh'}}>
 
-            <div className={`col-4 h-75 rounded-start fullCard${theme}`}>
+
+
+
+
+            <div className={`col-6 col-sm-5 col-md-4 col-xl-5 h-75 rounded-start fullCard${theme}`}>
               <div className='container p-0  h-100'>
                 <div className='row h-100'>
                   <div className='col-8'>
@@ -78,39 +80,45 @@ function NextDays() {
                   </div>
                 </div>
               </div>
-              
             </div>
 
-            <div className={`col-4 p-0 `}>
+
+
+
+            <div className={`col-6 col-sm-5 p-0 col-md-4 col-xl-3`}>
               <div className='container-fluid h-100 p-0'>
                 <div className={`h-75 dailyLeftContainer${theme}`}>
                   <div className='col-12 h-100 d-flex align-items-center justify-content-center'>
-                    <span style={{textTransform:'capitalize', letterSpacing:2, fontSize:'20px'}} className={`dailyContainerMidBottom${theme}`}>{item.weather}</span>
+                    <p style={{textTransform:'capitalize', letterSpacing:2, fontSize:'20px'}} className={`text-center pt-3 dailyContainerMidBottom${theme}`}>{item.weather}</p>
                   </div>
                 </div>
               </div>
             </div>
 
 
-            <div className={`col-4 p-0 `}>
+
+
+            <div className={`p-0 d-none d-sm-block col-sm-2 col-md-4 col-xl-4`}>
               <div className={`container-fluid h-100`}>
                 <div className={`row h-75 border-start text-center rounded-end dailyRightContainer${theme}`}>
-
-                  <div className={`col-4`}>
+                  <div className={`col-12 col-md-4 col-xl-6 col-xxl-4`}>
                     <div className='row mt-3'><span><img width={40} src={`/wind${theme}.png`}></img></span></div><hr className='mt-2 mb-2'/>
-                    <div><span className='fw-bold' style={{fontSize:'16px'}}>{item.wind} km/h</span></div>
+                    <div><span className='fw-bold' style={{fontSize:'14px'}}>{item.wind.toFixed(1)} <small>km/h</small></span></div>
                   </div>
-                  <div className={`col-4`}>
+                  <div className={`col-md-4 d-md-block d-none col-xl-6 col-xxl-4`}>
                     <div className='row mt-3'><span><img width={40} src={`/feel${theme}.png`}></img></span></div><hr className='mt-2 mb-2'/>
                     <div><span className='fw-bold' style={{fontSize:'16px'}}>{item.feel.toFixed()}°C</span></div>
                   </div>
-                  <div className={`col-4`}>
+                  <div className={`col-md-4 d-md-block d-none d-xl-none d-xxl-block`}>
                     <div className='row mt-3'><span><img width={40} src={`/clouds${theme}.png`}></img></span></div><hr className='mt-2 mb-2'/>
                     <div><span className='fw-bold' style={{fontSize:'16px'}}>% {item.clouds}</span></div>
                   </div>
                 </div>
               </div>
             </div>
+
+
+
 
 
           </div>
