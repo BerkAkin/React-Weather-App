bd1a451fc1cfb24587b7ff770a42597b8bcb68ca
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-11-26 03:17:02 +0300

    style: component is now repsonsive

--- weather/src/components/Temperature/index.js
+++ weather/src/components/Temperature/index.js
@@ -5,7 +5,6 @@ import Theme from '../../contexts/ThemeContext/ThemeContext'
 import Details from '../Details'
 import './style.css'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Temperature() {
@@ -16,17 +15,17 @@ function Temperature() {
     <>
       <div className={`rounded-start topContext${theme}`} style={{height:'86vh'}}>
         <div style={{height:'41vh'}} className='d-flex p-2'>
-          <div className={`container rounded innerTab${theme}`}>
+          <div className={`container-fluid rounded innerTab${theme}`}>
             <div className='row h-50 text-center'>
               <span>
                 <img height="200" src={data ? `http://openweathermap.org/img/wn/${data['list'][0]['weather'][0]['icon']}@4x.png`: " "}></img>
               </span>
             </div>          
-            <div className='row  mt-2 text-center' style={{fontSize:'1.6em', textTransform:'capitalize'}}>
+            <div className='row mt-2 text-center' style={{fontSize:'1.6em', textTransform:'capitalize'}}>
               <p>{data ? data['list'][0]['weather'][0]['description']:' '}</p>
             </div>
             <div className='row  justify-content-center'>
-              <p className='border rounded-pill w-25 text-center' style={{fontSize:'1.6em'}}>{data ? data['list'][0]['main']['temp'].toFixed(0):''} °C</p>
+              <p className='border rounded-pill w-50 text-center' style={{fontSize:'1.6em'}}>{data ? data['list'][0]['main']['temp'].toFixed(0):''} °C</p>
             </div>
             <div className='row '>
               <p className=' text-center'>Hissedilen Sıcaklık : {data ? data['list'][0]['main']['feels_like'].toFixed(0):''} °C</p>
